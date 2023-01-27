627fad60a00b7b4ddcb083961e906fc0786b167d
sud bond <jangrasudeep@gmail.com>
2023-01-27 15:46:04 +0530

    Added another method

--- src/com/java/leetcode/SingleNumber.java
+++ src/com/java/leetcode/SingleNumber.java
@@ -1,5 +1,7 @@
 package com.java.leetcode;
 
+import java.util.Stack;
+
 public class SingleNumber {
 	// method to find the single number in the given array
 	public int findSingleNumber(int[] nums) {
@@ -27,10 +29,24 @@ public class SingleNumber {
 		// finallly singleNumber will be returned;
 		return singleNumber;
 	}
+	// another method to find the single number
+	public int findSingleNumber1(int[] nums) {
+		Stack<Integer> stack = new Stack<>();
+		for(int i=0;i<nums.length; i++) {
+			if(nums[i]==stack.peek()) {
+				stack.pop();
+			} else {
+				stack.push(nums[i]);
+			}
+		}
+		if(!stack.isEmpty()) return stack.peek();
+		return -1;
+		
+	}
 	// driver method
 	public static void main(String[] args) {
 		// integer array to be passed as an argument
-		int[] array1 = { 2, 2, 1 };
+		int[] array1 = { 2, 2, 1,1 };
 		int[] array2 = { 4, 2, 1, 2, 1 };
 		int[] array3 = { 2, 2, 1 };
 		int[] array4 = { 1, 2, 1, 4, 4, 5, 2 };
