316865766371dd484cf8940ff2851cebfded1969
sud bond <jangrasudeep@gmail.com>
2023-02-09 16:24:02 +0530

    Some more comments were added

--- src/com/java/leetcode/LargestNumberAfterSwappingByParity.java
+++ src/com/java/leetcode/LargestNumberAfterSwappingByParity.java
@@ -5,51 +5,48 @@ import java.util.Scanner;
 class LargestNumberAfterSwappingByParity {
 	// method for swapping two numbers
 	static String swap(String str, int i, int j) {
+		// string to store the swapped value
 		StringBuilder sb = new StringBuilder(str);
+		// setting the value at index j
 		sb.setCharAt(i, str.charAt(j));
+		// setting the value at index i
 		sb.setCharAt(j, str.charAt(i));
+		// returning the sb as a string
 		return sb.toString();
 	}
 
 	// method to get largest number
 	public int largestInteger(int num) {
-		// String will be used to represent the
-		// number in string
+		// String will be used to represent the number in string
 		String s = Integer.toString(num);
 		// Traversing the string
 		for (int i = 0; i < s.length(); i++) {
 			int maxi = s.charAt(i) - '0';
 			int idx = i;
-			// Check ith digit with all
-			// the remaining unoperated
-			// string to maximize the string
+			// Check ith digit with all the remaining unoperated string to maximize the
+			// string
 			for (int j = i + 1; j < s.length(); j++) {
-				// If both the ith and
-				// jth digit is odd
+				// If both the ith and jth digit is odd
 				if (maxi % 2 == 0 && (s.charAt(j) - '0') % 2 == 0) {
-					// If the jth digit is
-					// greater than the ith digit
+					// If the jth digit is greater than the ith digit
 					if (s.charAt(j) - '0' > maxi) {
 						maxi = s.charAt(j) - '0';
 						idx = j;
 					}
 				}
-				// If both ith and
-				// jth digit is even
+				// If both ith and jth digit is even
 				else if (maxi % 2 == 1 && (s.charAt(j) - '0') % 2 == 1) {
-					// If the jth digit is
-					// greater than ith digit.
+					// If the jth digit is greater than ith digit.
 					if (s.charAt(j) - '0' > maxi) {
 						maxi = s.charAt(j) - '0';
 						idx = j;
 					}
 				}
 			}
-			// Swap the largest digit to the right
-			// of ith digit with ith digit
+			// Swap the largest digit to the right of ith digit with ith digit
 			s = swap(s, i, idx);
 		}
-		// Convert string into integer
+		// return and convert string into integer
 		return Integer.parseInt(s);
 	}
 
